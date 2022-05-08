6694af9a34a6f87e4b28bd8aa2910876de64b426
Snisarenko21 <sepalena@gmail.com>
2022-05-08 15:48:37 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,21 @@
-// ==========импортируем библиотеки 
-import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// console.log(Vimeo);
+
+const STORAGE_KEY = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+
+player.setVolume(0);
+    
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+// створюємо функцію, яка перевіряє чи є ключ в локалсторідж
+function onTimeUpdate(data) {
+    
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+}
+   
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+    

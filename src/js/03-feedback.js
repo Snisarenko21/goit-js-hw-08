6b23328319cec3b6497d235b2e28ad81bffa1b36
Snisarenko21 <sepalena@gmail.com>
2022-05-08 16:03:38 +0300

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,54 @@
+import throttle from 'lodash.throttle';
 
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('.feedback-form input'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+refs.form.addEventListener('input', throttle(onFormDataInput, 500));
+ 
+populateFormData();
+
+function onFormSubmit(event) {
+    
+    event.preventDefault();
+    
+    if (!formData[refs.input.name] || !formData[refs.textarea.name]) {
+        console.log("заполните все поля формы");
+    }
+    else {
+        
+    console.log(formData);
+    console.log('отправляем форму');
+     
+    event.currentTarget.reset();
+        
+    localStorage.removeItem(STORAGE_KEY);
+    formData[refs.input.name] = '';
+    formData[refs.textarea.name] = '';
+    
+    }
+};
+
+function onFormDataInput(e) {
+
+    formData[e.target.name] = e.target.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function populateFormData() {
+    const savedMasseg = localStorage.getItem(STORAGE_KEY);
+    const parsedsavedMasseg = JSON.parse(savedMasseg)
+    if (savedMasseg) {
+        refs.form.elements.message.value = parsedsavedMasseg.message || "";
+        refs.form.elements.email.value = parsedsavedMasseg.email || "";
+    }
+}
